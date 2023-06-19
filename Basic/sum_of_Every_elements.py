63bf437937327b5c4564986f5fdc0d78997f16e1
rakesh253 <rakesh25311@gmail.com>
2023-06-19 15:54:40 +0530

    few updates in basic

---
+++ Basic/sum_of_Every_elements.py
@@ -0,0 +1,9 @@
+#Sum Of Every Element in a List
+def sum_of_elements(nums):
+    return sum(nums)
+
+# Example usage
+nums = [1, 2, 3, 4, 5]
+result = sum_of_elements(nums)
+print(result)  # Output: 15
+#Code by @Rakesh Roshan Rath
